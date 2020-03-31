25debab51c40f539ef7ed607b2146f76503b6416
azmainamin <mdazmainamin@gmail.com>
2020-03-31 16:36:19 -0400

    Updates

---
+++ precache-manifest.f281ca15c224f4dfda2edcf550e0afd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1399e1b7ab323b1390daf5b340a6dd56",
+    "url": "/csv-pretty-view/index.html"
+  },
+  {
+    "revision": "0827aa5e9e1b4e433788",
+    "url": "/csv-pretty-view/static/css/main.20a564d2.chunk.css"
+  },
+  {
+    "revision": "a327c0f5e389e8f0000a",
+    "url": "/csv-pretty-view/static/js/2.d7f64014.chunk.js"
+  },
+  {
+    "revision": "7fb2762998a5a5d940c00f0ce950c258",
+    "url": "/csv-pretty-view/static/js/2.d7f64014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0827aa5e9e1b4e433788",
+    "url": "/csv-pretty-view/static/js/main.999748b2.chunk.js"
+  },
+  {
+    "revision": "a1e485e1b83bc16636a8",
+    "url": "/csv-pretty-view/static/js/runtime-main.16e7f094.js"
+  },
+  {
+    "revision": "d23c834fb3347f59d08819c5ac9e844f",
+    "url": "/csv-pretty-view/static/media/app_center_image.d23c834f.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/csv-pretty-view/precache-manifest.f281ca15c224f4dfda2edcf550e0afd9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/csv-pretty-view/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
